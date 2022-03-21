4f1ffd83e71da8ac412a527419bcf9edc9e343cf
fsdtrinings <87074240+fsdtrinings@users.noreply.github.com>
2022-03-21 15:03:51 +0530

    Add class assignment

---
+++ Jang-Batch2/src/a1/Assignment1.java
@@ -0,0 +1,29 @@
+package a1;
+
+/*
+  complete the code so that class can compile and generate output 
+   NOTE : You cannot change the code structure
+   
+ */
+public class Assignment1 {
+
+	int arr[] = {45,78,95,43,15,37,150,20};
+	
+	public static void main(String[] args) {
+	
+		 // call method getElementPosition to find out whether element is present in the list or not
+		
+		// call displayMethod to print appropriate output
+	}
+	
+	
+	public int getElementPosition(int searchNumber)
+	{
+		// method should return the position of the element otherwise return -1
+	}
+	
+	public void displayInfomation(int searchNumber,int position)
+	{
+		// if position is -1 then output should <<searchNumber>> not exist in the list
+	}
+}
