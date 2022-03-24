9f3bdbbc75a9622ef588f40fd96e16abfe98a81e
fsdtrinings <87074240+fsdtrinings@users.noreply.github.com>
2022-03-24 15:58:08 +0530

    Assignment 2 Day 3

---
+++ Jang-Batch2/src/task1/Assignment2.java
@@ -0,0 +1,33 @@
+package task1;
+
+public class Assignment2 {
+	
+	String emails[] = {"ramesh@gmail.com","suresh@yahoo.com","kapil@gmail.com","mayank@linkedin.com","neha@linkedin.com"};
+
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Enter Domain name (for filteration) :- ");
+		String domainName = _____________________; // get it from user
+		
+	}
+	
+	// optional is :- use List instead of array
+	public String[] filterEmailByDomainName(String domainName)
+	{
+		// code to filter emails , based on domain name passed in the argument
+		
+		// put filtered emails in temporary string array and return that array
+		return ______________;
+	}
+	
+	public void printArray(_________________)
+	{
+		// print filtered emails based on enhance for loop only
+	}
+
+}
+
+/*
+ * hint :- search strings endsWith() 
+ * *
